f31f3af579006efd842a5fe6760c1f0db28df68c
onurcan-ada <sleepiaapp@gmail.com>
2021-08-11 23:20:35 +0300

    glue link is corrected

--- src/test/java/com/FinWorks/runners/FailedTestRunner.java
+++ src/test/java/com/FinWorks/runners/FailedTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"html:target/failed-html-report"},
         features = "@target/rerun.txt",
-        glue = "com/vytrack/step_definitions"
+        glue = "com/FinWorks/step_definitions"
 )
 public class FailedTestRunner {
 }

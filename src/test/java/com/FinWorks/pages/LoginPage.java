4c075a15aa066216fc69001f210fc62f3a00687a
Evelin <evelindacanovas@gmail.com>
2021-08-13 01:47:29 +0200

    LoginPage dynamic method for get subtitle pages created Inbox subtitle in main page locator added

--- src/test/java/com/FinWorks/pages/LoginPage.java
+++ src/test/java/com/FinWorks/pages/LoginPage.java
@@ -3,6 +3,7 @@ package com.FinWorks.pages;
 
 
 import com.FinWorks.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -25,12 +26,18 @@ import org.openqa.selenium.support.PageFactory;
         @FindBy(xpath = "//*[@id='wrapwrap']/main/div/form/div[3]/button")
         public WebElement submit;
 
+        @FindBy (xpath = "//li[text()='#Inbox']")
+        public WebElement inboxPageSubtitle;
 
         public void login(String userNameStr, String passwordStr) {
             userName.sendKeys(userNameStr);
             password.sendKeys(passwordStr);
             submit.click();
             // verification that we logged
+        }
+        public WebElement getPageSubtitleText(String subtitleName){
+            WebElement element = Driver.get().findElement(By.xpath("//li[text()='" + subtitleName + "']"));
+            return element;
         }
 
 
--- src/test/java/com/FinWorks/runners/CukesRunner.java
+++ src/test/java/com/FinWorks/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/FinWorks/step_definitions",
         dryRun = false,
-        tags = "@login"
+        tags = "@wip"
 )
 public class CukesRunner {
 }

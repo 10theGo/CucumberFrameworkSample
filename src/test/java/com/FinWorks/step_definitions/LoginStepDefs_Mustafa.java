db13ca607e69c905bb3ccaaa94299bf6ed9dfa2e
mustafa <yusufkenan2604@gmail.com>
2021-08-13 13:18:05 +0200

    LoginPage_Mustafa Login_Mustafa.feature LoginStepDefs_Mustafa add...

---
+++ src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Mustafa.java
@@ -0,0 +1,31 @@
+package com.FinWorks.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefs_Mustafa {
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("I enter user name and password for {string}")
+    public void i_enter_user_name_and_password_for(String string) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("I click login button")
+    public void i_click_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("I should be able to login")
+    public void i_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+}

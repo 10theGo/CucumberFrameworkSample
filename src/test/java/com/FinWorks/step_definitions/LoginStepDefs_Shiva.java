7864456bb8b6f9c5de2ce95f0b6f5818898bf5c0
shiva <shivapourbehzad@gmail.com>
2021-08-15 21:41:23 +0200

    LoginStepDefs_Shiva is updated

--- src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
+++ src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
@@ -1,20 +1,27 @@
 package com.FinWorks.step_definitions;
 
+import com.FinWorks.pages.LoginPage_Shiva;
+import com.FinWorks.utilities.BrowserUtils;
+import com.FinWorks.utilities.ConfigurationReader;
+import com.FinWorks.utilities.Driver;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefs_Shiva {
 
-    @Given("the user logged in as {string}")
-    public void the_user_logged_in_as(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @When("the user enters the valid information {string} {string}")
+    public void the_user_enters_the_valid_information(String username, String password) {
+        new LoginPage_Shiva().login(username,password);
+
     }
 
-    @When("the user enters the {string} {string}")
-    public void the_user_enters_the(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
     }
 
-}
+
+
+
+
+

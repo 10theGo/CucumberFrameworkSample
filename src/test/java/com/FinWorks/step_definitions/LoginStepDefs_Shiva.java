5cea23bb8d2cb588b73bb5de139f9656d2207f74
shiva <shivapourbehzad@gmail.com>
2021-08-15 22:10:56 +0200

    LoginStepDefs_Shiva is completed

--- src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
+++ src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
@@ -11,14 +11,25 @@ import org.junit.Assert;
 
 public class LoginStepDefs_Shiva {
 
-
     @When("the user enters the valid information {string} {string}")
     public void the_user_enters_the_valid_information(String username, String password) {
         new LoginPage_Shiva().login(username,password);
 
     }
 
+    @When("the user enters the invalid information {string} or {string}")
+    public void theUserEntersTheInvalidInformationOr(String username, String password) {
+        new LoginPage_Shiva().login(username,password);
+
     }
+
+    @Then("the user should not be able to Login")
+    public void theUserShouldNotBeAbleToLogin() {
+        BrowserUtils.waitFor(4);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("Login | Best solution for startups", actualTitle);
+    }
+}
 
 
 

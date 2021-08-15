141f209ae521b5b24a9d91eb649f175f0e477d8a
shiva <shivapourbehzad@gmail.com>
2021-08-15 20:51:23 +0200

    LoginStepDefs_Shiva in process

--- src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
+++ src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
@@ -1,4 +1,20 @@
 package com.FinWorks.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
 public class LoginStepDefs_Shiva {
+
+    @Given("the user logged in as {string}")
+    public void the_user_logged_in_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user enters the {string} {string}")
+    public void the_user_enters_the(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }

c61e2708252102ba67b79e3bff96d579b6f60cb3
shiva <shivapourbehzad@gmail.com>
2021-08-18 23:40:01 +0200

    added Given and Then to LoginStepDefs_Shiva

--- src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
+++ src/test/java/com/FinWorks/step_definitions/LoginStepDefs_Shiva.java
@@ -11,6 +11,12 @@ import org.junit.Assert;
 
 public class LoginStepDefs_Shiva {
 
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        String url = ConfigurationReader.get("url");
+        //WebDriver driver = Driver.get();
+        Driver.get().get(url);
+    }
 
     @When("the user enters the valid information {string} {string}")
     public void the_user_enters_the_valid_information(String username, String password) {
@@ -29,6 +35,13 @@ public class LoginStepDefs_Shiva {
         BrowserUtils.waitFor(4);
         String actualTitle = Driver.get().getTitle();
         Assert.assertEquals("Login | Best solution for startups", actualTitle);
+    }
+
+    @Then("the user should be able to Login")
+    public void the_user_should_be_able_to_Login() {
+        BrowserUtils.waitFor(5);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("#Inbox - Odoo", actualTitle);
     }
 
 

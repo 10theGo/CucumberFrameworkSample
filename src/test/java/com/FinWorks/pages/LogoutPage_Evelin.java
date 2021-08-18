d7eabb7afc06a39836f45bb6e263bac4dcb3a953
Evelin <evelindacanovas@gmail.com>
2021-08-18 11:55:00 +0200

    LogoutPage_Evelin logout avatar and name button locators and get methods created

--- src/test/java/com/FinWorks/pages/LogoutPage_Evelin.java
+++ src/test/java/com/FinWorks/pages/LogoutPage_Evelin.java
@@ -1,4 +1,21 @@
 package com.FinWorks.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LogoutPage_Evelin extends DashboardPage {
+
+    @FindBy (css = "img-circle oe_topbar_avatar")
+    private WebElement logoutAvatarButton;
+
+    @FindBy (css =  "//*[@class='oe_topbar_name']")
+    private WebElement logoutNameButton;
+
+    public WebElement getLogoutNameButton() {
+        return logoutNameButton;
+    }
+
+    public WebElement getLogoutAvatarButton(){
+        return logoutAvatarButton;
+    }
 }

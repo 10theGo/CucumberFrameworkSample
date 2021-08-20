0953b3ce69f074855106a0df2572bd0ddab44920
AdnanRumi <mugterib@gmail.com>
2021-08-20 09:53:30 +0200

    First commit

---
+++ src/test/java/com/FinWorks/pages/LoginPage_AY.java
@@ -0,0 +1,34 @@
+package com.FinWorks.pages;
+
+import com.FinWorks.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage_AY extends BasePage{
+
+    public LoginPage_AY() {
+
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+    @FindBy(xpath = "//input[@id='login']")
+    private WebElement emailInputBox;
+
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement passwordInputBox;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(xpath = "//a[@href='/web/reset_password?']")
+    private WebElement resetPassword;
+
+    @FindBy(xpath = "//p[contains(text(),'Wrong login/password')]")
+    private WebElement failedLoginMessage;
+
+
+
+
+
+}

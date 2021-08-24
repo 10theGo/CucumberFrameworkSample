69978e594514671add0c2bd1d0f62f628b2e610f
burhanalioglu <burhanalioglu@gmail.com>
2021-08-24 11:06:40 +0300

    Notes func test scenarios and some minor changes over file names added

--- src/test/java/com/FinWorks/pages/DiscussPage_Burhan.java
+++ src/test/java/com/FinWorks/pages/DiscussPage_Burhan.java
@@ -3,7 +3,7 @@ package com.FinWorks.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class DiscussPageBurhan extends BasePage {
+public class DiscussPage_Burhan extends BasePage {
     @FindBy(xpath = "//span[.='Inbox']")
     public WebElement inboxTab;
 }

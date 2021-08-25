d26ad1e9acfc2bf3ac189188e863d45483ca3533
AdnanRumi <mugterib@gmail.com>
2021-08-25 10:35:35 +0200

    Notes and taking notes pages more work done

--- src/test/java/com/FinWorks/pages/TakingNotesPage_AY.java
+++ src/test/java/com/FinWorks/pages/TakingNotesPage_AY.java
@@ -1,4 +1,14 @@
 package com.FinWorks.pages;
 
+import com.FinWorks.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class TakingNotesPage_AY {
+
+    public TakingNotesPage_AY() {
+
+        PageFactory.initElements(Driver.get(),this);
+    }
+
+
 }

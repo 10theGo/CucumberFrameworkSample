d35b58d8f16e88ee3269d903b6c221490e20fe1f
burhanalioglu <76847129+burhanalioglu@users.noreply.github.com>
2021-08-27 01:13:16 +0300

    Notes Functionality is done (#37)
    * Notes functionality step defs added
    
    * Notes functionality step defs added
    
    Co-authored-by: onurcan-ada <80535146+onurcan-ada@users.noreply.github.com>

--- src/test/java/com/FinWorks/pages/NotesPage_Burhan.java
+++ src/test/java/com/FinWorks/pages/NotesPage_Burhan.java
@@ -3,6 +3,7 @@ package com.FinWorks.pages;
 import com.FinWorks.utilities.BrowserUtils;
 import com.FinWorks.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -30,6 +31,16 @@ public class NotesPage_Burhan extends BasePage {
 
     @FindBy(css = ".note-editable.panel-body")
     private WebElement noteEditablePanelBody;
+
+    @FindBy(xpath = "//button[@class='btn btn-primary btn-sm o_form_button_save']")
+    private WebElement NoteSaveButton;
+
+    @FindBy(className = "o_form_sheet")
+    private WebElement savedNoteSheet;
+
+    @FindBy(xpath = "//button[@title='Log or schedule an activity']")
+    private WebElement scheduleActivity;
+
 
     public void clickOnCreateNoteButton() {
         createNoteButton.click();
@@ -73,4 +84,34 @@ public class NotesPage_Burhan extends BasePage {
     public String getNoteText() {
         return noteEditablePanelBody.getText();
     }
+
+
+    public void selectDateType(String dateType) {
+        WebElement dateTypeButton = Driver.get().findElement(By.xpath("//button[contains(text(),'" + dateType + "')][contains(@class,'o_arrow_button')]"));
+        dateTypeButton.click();
+    }
+
+    public boolean isDateTypeDisabled(String dateType) {
+        WebElement dateTypeButton = Driver.get().findElement(By.xpath("//button[contains(text(),'" + dateType + "')][contains(@class,'o_arrow_button')]"));
+        String disabledValue = dateTypeButton.getAttribute("disabled");
+        System.out.println("disabledValue = " + disabledValue);
+        return disabledValue.equals("true");
+    }
+
+    public void clickOnSaveNote() {
+        NoteSaveButton.click();
+    }
+
+    public boolean isNotePageDisplayed() {
+        return savedNoteSheet.isDisplayed();
+    }
+
+    public void waitUntilScheduleActivityAppears() {
+        try {
+            BrowserUtils.waitForVisibility(scheduleActivity, 3);
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+    }
+
 }

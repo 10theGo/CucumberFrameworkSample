d6f494a69fa58c17a9bc4a365071dcd7fe25a609
burhanalioglu <burhanalioglu@gmail.com>
2021-09-09 12:18:52 +0300

    Vehicles module new files added

--- src/test/java/com/FinWorks/pages/NavBarPage_Burhan.java
+++ src/test/java/com/FinWorks/pages/NavBarPage_Burhan.java
@@ -4,6 +4,7 @@ import com.FinWorks.utilities.BrowserUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NavBarPage_Burhan extends BasePage {
@@ -11,14 +12,28 @@ public class NavBarPage_Burhan extends BasePage {
     @FindBy(className = "oe_menu_text")
     private List<WebElement> navBarTabs;
 
+    @FindBy(xpath = "//li[@id = 'menu_more_container']/a")
+    private WebElement menuTab;
+
 
     public void clickOnNavBarTab(String tabName) {
         BrowserUtils.waitForPageToLoad(3);
+        if (menuTab.isDisplayed() && !navBarTabTexts().contains(tabName)) {
+            menuTab.click();
+        }
         for (WebElement navBarTab : navBarTabs) {
             if (navBarTab.getText().trim().equals(tabName)) {
                 navBarTab.click();
             }
         }
+    }
+
+    private List<String> navBarTabTexts() {
+        List<String> textList = new ArrayList<>();
+        for (WebElement navBarTab : navBarTabs) {
+            textList.add(navBarTab.getText().trim());
+        }
+        return textList;
     }
 
 }
---
+++ src/test/resources/features/Vehicles.feature
@@ -0,0 +1,13 @@
+Feature: Fleet Module
+  As a PosManager I should be able to navigate to the fleet module
+
+  Background:
+    Given The user is on the login page
+    And The user logs in as "posmanager"
+
+  @brhn
+  Scenario: PosManager should successfully land on Vehicles page
+    When The user navigates to "Fleet" tab_brhn
+    Then The user is on the Vehicles Page_brhn
+
+  Scenario: PosManager should be able to navigate between Vehicle modules
